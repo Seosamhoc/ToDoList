6871149f9ffa8a80c1f4256c2250af387b6592b0
SK <seosamh.ocinneide@gmail.com>
2019-04-14 20:19:38 +0100

    Added unit testing framework

---
+++ ToDoListTests/UnitTest1.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+using ToDoList;
+
+namespace ToDoListTests
+{
+    public class UnitTest1
+    {
+        //[Fact]
+        //public void Test1()
+        //{
+
+        //}
+
+        //[Theory]
+        //[InlineData("", "")]
+        //public void Test2(string teststring, string expected)
+        //{
+        //    //Arrange /\/\/\
+        //    //Act
+
+        //    //Assert
+        //    Assert.Equal(expected, teststring);
+        //}
+    }
+}
